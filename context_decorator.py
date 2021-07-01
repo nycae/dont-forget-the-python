3a19fc7b07b35a0f3258c8d83e7d0991c6252dc0
Roberto Plaza <rplaza@topdoctors.es>
2021-07-01 11:50:00 +0200

    First commit

---
+++ context_decorator.py
@@ -0,0 +1,38 @@
+import contextlib
+import functools
+
+
+@contextlib.contextmanager
+def my_context():
+    print("Setup context")
+    try:
+        yield "Something"
+    except Exception as ex:
+        print("Rollback")
+    else:
+        print("Success")
+    finally:
+        print("Teardown")
+
+
+def context_decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if context := kwargs.pop('context', None):
+            return func(*args, context=context, **kwargs)
+        else:
+            with my_context() as context:
+                return func(*args, context=context, **kwargs)
+    return wrapper
+
+
+@context_decorator
+def test(context):
+    print(context)
+    if context == 42:
+        raise Exception()
+
+
+if __name__ == '__main__':
+    test()
+
